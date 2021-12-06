b530f59c605557bc4572736248148c336f48b7d8
Danny <danny.affiliatemarketing@gmail.com>
2021-12-06 19:16:48 -0300

    add index.html

---
+++ myScript.js
@@ -0,0 +1,12 @@
+//create an array with the different options: rock, paper scissors.
+const userOptions = ["rock", "paper", "scissors"]; 
+//make a function that randomly selects one of the strings.
+function 
+//let the function run when the user enters a valid option.
+//let the function be named computerPlay
+// create a variable to store the users' choice: userInput
+// If users' input is valid - trigger a function that produces a random computer output from the array userOptions and store it 
+//in a variable called randomOutput
+//Once generated. Compare the randomOutput to the userInput using < >. 
+//Establish the winners. Rock >scissors. scissors>paper, paper>rock
+//If userInput is greater than randomOutput alert "you win", else "you lose"
