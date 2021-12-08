f404f885e8487c6a2c60e0b66e0b21d7c20a5ef8
Danny <danny.affiliatemarketing@gmail.com>
2021-12-08 17:55:14 -0300

    myScript.js

--- myScript.js
+++ myScript.js
@@ -1,36 +1,30 @@
-//a function that randomly selects a string
+const move=["rock", "paper", "scissors"]
+
 function computerPlay() {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
+    return move[Math.floor(Math.random() * move.length)]
 }
 
-//establisth variables 
-let playerSelection = "rock";
-//working, I think I was calling the same function twice. When I logged I got two different values. 
-let computerSelection = computerPlay();
-console.log(computerSelection);
-
-//create a function to compare playerSelection to computerSelection
-function playRound(playerSelection, computerSelection) {
-// your code here! - if statements not working properly - force playerSelection to lower case to desensitize it. ".toLowerCase()" 
-        computerSelection = computerPlay().toLowerCase();
-        playerSelection = playerSelection.toLowerCase();
-        
-        if (computerSelection == playerSelection) {
-            return ("Tie game!");
-        }else if(playerSelection==="rock" && computerSelection === "paper"){
-            return ("You lose. Paper beats rock.")
-        }else if(playerSelection==="rock" && computerSelection === "scissors"){
-            return result ("You win. Rock beats scissors.")
-        }else if(playerSelection==="scissors" && computerSelection === "rock"){
-            return result ("You lose. Rock beats scissors.")
-        }else if(playerSelection==="scissors" && computerSelection === "paper"){
-            return result ("You win. Scissors beats paper.")
-        }else if(playerSelection==="paper" && computerSelection === "rock"){
-            return result ("You win. Paper beats rock.")
-        }else if(playerSelection==="paper" && computerSelection === "scissors"){
-            return result ("You lose. Scissors beats paper.")
-        }
+function playerPlay() {
+    return move[Math.floor(Math.random() * move.length)]
 }
-  //calling the function
-  console.log(playRound());
+
+function playRound() {
+    const playerSelection = playerPlay()
+    const computerSelection = computerPlay()
+
+    if (playerSelection === "rock" && computerSelection === "paper")
+        return "Sorry, you lost. Paper beats rock!"
+    else if (playerSelection === "rock" && computerSelection === "rock")
+        return "You tied"
+    else if (playerSelection === "rock" && computerSelection === "scissors")
+        return "You won! Rock beats scissors!"
+    
+}
+
+function game() {
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+    console.log(playRound())
+}
+console.log(game())
