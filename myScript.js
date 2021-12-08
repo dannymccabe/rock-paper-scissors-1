4d6cad88045b16d7c36928b4ef87ec4ee3e0a409
Danny <danny.affiliatemarketing@gmail.com>
2021-12-08 18:10:59 -0300

    myScript.js

--- myScript.js
+++ myScript.js
@@ -12,13 +12,20 @@ function playRound() {
     const playerSelection = playerPlay()
     const computerSelection = computerPlay()
 
-    if (playerSelection === "rock" && computerSelection === "paper")
-        return "Sorry, you lost. Paper beats rock!"
-    else if (playerSelection === "rock" && computerSelection === "rock")
+    if (playerSelection == computerSelection)
         return "You tied"
+    else if(playerSelection === "rock" && computerSelection === "paper")
+        return "Sorry, you lost. Paper beats rock!"
     else if (playerSelection === "rock" && computerSelection === "scissors")
         return "You won! Rock beats scissors!"
-    
+    else if (playerSelection === "paper" && computerSelection === "scissors")
+        return "Sorry, you lost! Scissors beats paper!"
+    else if (playerSelection === "paper" && computerSelection === "rock")
+        return "You won! Paper beats rock!"
+    else if (playerSelection === "scissors" && computerSelection === "rock")
+        return "Sorry, you lost! Rock beats scissors!"
+    else if (playerSelection === "scissors" && computerSelection === "paper")
+        return "You won! Scissors beats paper!"
 }
 
 function game() {
@@ -26,5 +33,7 @@ function game() {
     console.log(playRound())
     console.log(playRound())
     console.log(playRound())
+    console.log(playRound())
 }
 console.log(game())
+//completed the game by copying a youtube tutorial and finishing the if..else() statements. I was following the same logic but this one worked and mine didn't.
